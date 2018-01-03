91c1fb9d91af683f7ef9d5ddff13a74b41029c9a
Iacopo Spalletti <i.spalletti@nephila.it>
2018-01-03 12:00:20 +0100

    Revert "Fix swappable models"
    This reverts commit 9592da41dd858990735bebc1e13831b239ad4765.

--- filer/models/imagemodels.py
+++ filer/models/imagemodels.py
@@ -15,39 +15,39 @@ from .abstract import BaseImage
 
 logger = logging.getLogger("filer")
 
-if getattr(settings, 'FILER_IMAGE_MODEL') == 'filer.Image':
-    # This is the standard Image model which can be swapped for a custom model using FILER_IMAGE_MODEL setting
-    class Image(BaseImage):
-        date_taken = models.DateTimeField(_('date taken'), null=True, blank=True,
-                                          editable=False)
-        author = models.CharField(_('author'), max_length=255, null=True, blank=True)
-        must_always_publish_author_credit = models.BooleanField(_('must always publish author credit'), default=False)
-        must_always_publish_copyright = models.BooleanField(_('must always publish copyright'), default=False)
 
-        class Meta(BaseImage.Meta):
-            swappable = 'FILER_IMAGE_MODEL'
-            if GTE_DJANGO_1_10:
-                default_manager_name = 'objects'
+# This is the standard Image model which can be swapped for a custom model using FILER_IMAGE_MODEL setting
+class Image(BaseImage):
+    date_taken = models.DateTimeField(_('date taken'), null=True, blank=True,
+                                      editable=False)
+    author = models.CharField(_('author'), max_length=255, null=True, blank=True)
+    must_always_publish_author_credit = models.BooleanField(_('must always publish author credit'), default=False)
+    must_always_publish_copyright = models.BooleanField(_('must always publish copyright'), default=False)
 
-        def save(self, *args, **kwargs):
-            if self.date_taken is None:
-                try:
-                    exif_date = self.exif.get('DateTimeOriginal', None)
-                    if exif_date is not None:
-                        d, t = exif_date.split(" ")
-                        year, month, day = d.split(':')
-                        hour, minute, second = t.split(':')
-                        if getattr(settings, "USE_TZ", False):
-                            tz = get_current_timezone()
-                            self.date_taken = make_aware(datetime(
-                                int(year), int(month), int(day),
-                                int(hour), int(minute), int(second)), tz)
-                        else:
-                            self.date_taken = datetime(
-                                int(year), int(month), int(day),
-                                int(hour), int(minute), int(second))
-                except Exception:
-                    pass
-            if self.date_taken is None:
-                self.date_taken = now()
-            super(Image, self).save(*args, **kwargs)
+    class Meta(BaseImage.Meta):
+        swappable = 'FILER_IMAGE_MODEL'
+        if GTE_DJANGO_1_10:
+            default_manager_name = 'objects'
+
+    def save(self, *args, **kwargs):
+        if self.date_taken is None:
+            try:
+                exif_date = self.exif.get('DateTimeOriginal', None)
+                if exif_date is not None:
+                    d, t = exif_date.split(" ")
+                    year, month, day = d.split(':')
+                    hour, minute, second = t.split(':')
+                    if getattr(settings, "USE_TZ", False):
+                        tz = get_current_timezone()
+                        self.date_taken = make_aware(datetime(
+                            int(year), int(month), int(day),
+                            int(hour), int(minute), int(second)), tz)
+                    else:
+                        self.date_taken = datetime(
+                            int(year), int(month), int(day),
+                            int(hour), int(minute), int(second))
+            except Exception:
+                pass
+        if self.date_taken is None:
+            self.date_taken = now()
+        super(Image, self).save(*args, **kwargs)
