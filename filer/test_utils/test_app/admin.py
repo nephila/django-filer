4adcbc00aa7544dfbfdb4c7c7c241b43c55b8ca0
Iacopo Spalletti <i.spalletti@nephila.it>
2018-04-11 16:59:12 +0200

    Add test models

--- filer/test_utils/test_app/admin.py
+++ filer/test_utils/test_app/admin.py
@@ -3,10 +3,14 @@ from __future__ import absolute_import
 
 from django.contrib import admin
 
-from .models import MyModel
+from .models import MyModel, InlineModel
 
+
+class InlineModelInline(admin.TabularInline):
+    model = InlineModel
 
 class MyModelAdmin(admin.ModelAdmin):
     model = MyModel
+    inlines = (InlineModelInline,)
 
 admin.site.register(MyModel, MyModelAdmin)
--- filer/test_utils/test_app/models.py
+++ filer/test_utils/test_app/models.py
@@ -13,3 +13,8 @@ class MyModel(models.Model):
     general = FilerFileField(related_name='test_file')
     image = FilerImageField(related_name='test_image')
     folder = FilerFolderField(related_name='test_folder')
+
+
+class InlineModel(models.Model):
+    parent = models.ForeignKey(MyModel, on_delete=models.CASCADE)
+    image = FilerImageField(related_name='test_image2')
